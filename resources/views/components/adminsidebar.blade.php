c73512684b6958cb62f6564e4ede97b9b1c787e8
s-a-rafi <sarafi3258@gmail.com>
2025-10-21 06:22:02 +0600

    up

--- resources/views/components/adminsidebar.blade.php
+++ resources/views/components/adminsidebar.blade.php
@@ -18,7 +18,7 @@
 
     <li class="dropdown">
       <a href="#" onclick="toggleSubmenu(event)" title="Stations" id="drop">
-      <i class="fa fa-calendar"></i>
+        <i class="fa fa-calendar"></i>
         <span>Schedule</span>
         <svg class="arrow" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
           fill="#0000F5">
@@ -27,12 +27,13 @@
       </a>
       <ul class="submenu">
         <div>
-          <li class="{{request()->routeIs('schedules.create')?'active':''}}"><a href="{{ route('schedules.create') }}" title="Booking" id="sub">
+          <li class="{{request()->routeIs('schedules.create')?'active':''}}"><a href="{{ route('schedules.create') }}"
+              title="Booking" id="sub">
               <i class="fa-regular fa-clock"></i>
               <span>Create shedule</span>
             </a>
           </li>
-          <li><a href="#" title="Booking" id="sub">
+          <li><a href="{{ route('schedules.index') }}" title="All schedules" id="sub">
               <i class="fa-solid fa-calendar-days"></i>
               <span>All schedules</span>
             </a>
@@ -42,28 +43,90 @@
     </li>
 
 
-    <li>
-      <a href="{{ route('buses.index') }}" title="Bus Entry">
-        <i class="fa-solid fa-bus"></i>
+
+    <li class="dropdown">
+      <a href="#" onclick="toggleSubmenu(event)" title="Buses" id="drop">
+        <i class="fa fa-bus"></i>
         <span>Buses</span>
+        <svg class="arrow" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+          fill="#0000F5">
+          <path d="M480-360 280-560h400L480-360Z" />
+        </svg>
       </a>
+      <ul class="submenu">
+        <div>
+          <li><a href="{{ route('buses.create') }}" title="Add New Bus" id="sub">
+              <i class="fa fa-bus" aria-hidden="true"></i>
+              <span>Add New Bus</span>
+            </a>
+          </li>
+          <li><a href="{{ route('buses.index') }}" title="All Buses" id="sub">
+              <i class="fa fa-bus-side"></i>
+              <span>All Buses</span>
+            </a>
+          </li>
+        </div>
+      </ul>
     </li>
-    <li>
-      <a href="{{ route('locations.index') }}" title="Booking">
-        <i class="fa-solid fa-bus"></i>
+
+
+
+    <li class="dropdown">
+      <a href="#" onclick="toggleSubmenu(event)" title="location" id="drop">
+        <i class="fa-solid fa-location-dot"></i>
         <span>location</span>
+        <svg class="arrow" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+          fill="#0000F5">
+          <path d="M480-360 280-560h400L480-360Z" />
+        </svg>
       </a>
+      <ul class="submenu">
+        <div>
+          <li><a href="{{ route('locations.create') }}" title="Add location" id="sub">
+              <i class="fa-solid fa-location-dot"></i>
+              <span>Add location</span>
+            </a>
+          </li>
+          <li><a href="{{ route('locations.index') }}" title="All locations" id="sub">
+              <i class="fa-solid fa-map"></i>
+              <span>All locations</span>
+            </a>
+          </li>
+        </div>
+      </ul>
     </li>
-    <li>
-      <a href="{{ route('routes.index') }}" title="Booking">
-        <i class="fa-solid fa-bus"></i>
+
+
+
+    <li class="dropdown">
+      <a href="#" onclick="toggleSubmenu(event)" title="route" id="drop">
+        <i class="fa-solid fa-route"></i>
         <span>Route</span>
+        <svg class="arrow" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+          fill="#0000F5">
+          <path d="M480-360 280-560h400L480-360Z" />
+        </svg>
       </a>
+      <ul class="submenu">
+        <div>
+          <li><a href="{{ route('routes.create') }}" title="Add route" id="sub">
+              <i class="fa-solid fa-route"></i>
+              <span>Add Route</span>
+            </a>
+          </li>
+          <li><a href="{{ route('routes.index') }}" title="All routes" id="sub">
+              <i class="fa-solid fa-list"></i>
+              <span>All Routes</span>
+            </a>
+          </li>
+        </div>
+      </ul>
     </li>
+
 
     <li class="dropdown">
       <a href="#" onclick="toggleSubmenu(event)" title="Stations" id="drop">
-        <i class="fa-solid fa-bus"></i>
+        <i class="fa-solid fa-ticket"></i>
         <span>Stations</span>
         <svg class="arrow" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
           fill="#0000F5">
@@ -105,7 +168,7 @@
     </li>
     <li>
       <a href="#" title="Caleder">
-        <i class="fa-solid fa-bus"></i>
+        <i class="fa-solid fa-calendar"></i>
         <span>Calender</span>
       </a>
     </li>
